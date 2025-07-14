9d68cc2fa995d3299c4b46d1681e4bd331b2d134
hafsoo <hafsariazturk9@gmail.com>
2025-07-14 12:06:09 +0500

    set server url in frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,7 +1,9 @@
-//export const server="http://localhost:8000/api/v2";
+
 export const server="https://eshop-bay-five.vercel.app/api/v2";
 
+ export const backend_url = "https://eshop-bay-five.vercel.app";
 
 
-//this backend url for multer upload folder now in project we you cloudinary
- export const backend_url="http://localhost:8000"
+ //this backend url for multer upload folder now in project we you cloudinary
+ //export const backend_url="http://localhost:8000"
+//export const server="http://localhost:8000/api/v2";
