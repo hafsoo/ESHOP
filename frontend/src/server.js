c567b3f94ceaba2bbc9c0802c4a9c3efdaa5ebc5
hafsoo <hafsariazturk9@gmail.com>
2025-07-14 23:10:35 +0500

    new sepate backend url updated in frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,7 +1,7 @@
 
-export const server="https://eshop-bay-five.vercel.app/api/v2";
+export const server="https://eshop-backend-azure.vercel.app/api/v2";
 
- export const backend_url = "https://eshop-bay-five.vercel.app";
+ export const backend_url = "https://eshop-backend-azure.vercel.app/";
 
 
  //this backend url for multer upload folder now in project we you cloudinary
